9138ac4e1290caa5b89b07d1828f655bba8abac3
kmcaloney15 <katymcaloney@gmail.com>
2022-07-20 17:41:03 -0400

    super user created

--- bookcollector/main_app/admin.py
+++ bookcollector/main_app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # import your models here
-from .models import Book
+from .models import Book, Review, Fandom
 
 # Register your models here.
 admin.site.register(Book)
+admin.site.register(Review)
+admin.site.register(Fandom)
--- bookcollector/main_app/models.py
+++ bookcollector/main_app/models.py
@@ -1,5 +1,25 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
+
+REVIEW = (
+  ('A', 'Great'),
+  ('B', 'Okay'),
+  ('C', 'Not my favorite'),
+)
+
+class Fandom(models.Model):
+  name = models.CharField(max_length=50)
+  description = models.TextField(max_length=200)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('fandoms_detail', kwargs={'pk': self.id})
+
+
+
 
 # Create your models here.
 class Book(models.Model):
@@ -14,3 +34,21 @@ class Book(models.Model):
     # Add this method
     def get_absolute_url(self):
         return reverse('detail', kwargs={'book_id': self.id})
+
+
+class Review(models.Model):
+  date = models.DateField('Review Date')
+  review = models.CharField(
+    'Review',
+    max_length=1,
+    choices=REVIEW,
+    default=REVIEW[0][0]
+  )
+  # create a book_id column in the db
+  book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
+
+  class Meta:
+    ordering = ['-date']
